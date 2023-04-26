3cd08ff9b7a0ca42da21c43e70a6ed2a94dff93a
Kaibu <notkaibu@gmail.com>
2023-04-26 13:34:20 +0200

    init next

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+console.log("[App.vue]", `Hello world from Electron ${process.versions.electron}!`)
+</script>
+
+<template>
+  <HelloWorld msg="Electron + Vite + Vue" />
+  <div class="flex-center">
+    Place static files into the <code>/public</code> folder
+    <img style="width: 2.4em; margin-left: .4em;" src="/logo.svg" alt="Logo">
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+
+</template>
+
