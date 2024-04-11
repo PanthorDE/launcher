17d61a928601f2c2b4bf6181d1841b3f6a40fd8e
Kaibu <notkaibu@gmail.com>
2024-04-11 22:16:53 +0200

    Windows notifications fixed

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -34,7 +34,7 @@ autoUpdater.checkForUpdatesAndNotify()
 if (release().startsWith('6.1')) app.disableHardwareAcceleration();
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName());
+if (process.platform === 'win32') app.setAppUserModelId("panthor.de");
 
 if (!app.requestSingleInstanceLock()) {
   app.quit();
